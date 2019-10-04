a3734e7bf2e71024805cc9e0bacf3a54466809db
Rowan <emilyremy@gmail.com>
2019-10-04 13:32:15 -0600

    Prototype completed

---
+++ Capstone/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Camera playerCamera = null;
+    public float limitY = 60.0f;
+    public float cameraSmooth = 10.0f;
+    public float speed = 10.0f;
+
+    private float translation;
+    private float straffe;
+    private float lookVertical = 0.0f;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector3.zero;
+        rb.MovePosition(rb.transform.position += (rb.transform.forward * Input.GetAxis("Vertical") + rb.transform.right * Input.GetAxis("Horizontal")) * speed * Time.deltaTime);
+
+        lookVertical += Input.GetAxisRaw("Mouse Y") * cameraSmooth * Time.deltaTime;
+        lookVertical = Mathf.Clamp(lookVertical, -limitY, limitY);
+        playerCamera.transform.localRotation = Quaternion.AngleAxis(-lookVertical, Vector3.right);
+
+        rb.transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * cameraSmooth * Time.deltaTime);
+    }
+}
